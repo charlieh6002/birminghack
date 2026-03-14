2a38bb1f4431c317b4581ee49392721375cb3087
erinn <erin.a.caine@gmail.com>
2026-03-14 12:53:38 +0000

    added filefixer attempt 1

---
+++ filefixer-attempt1.py
@@ -0,0 +1,17 @@
+import pikepdf
+import os
+
+broken_file = "broken1.pdf"
+os.remove("repaired_pdf.pdf")
+new_file = open("repaired_pdf.pdf", "x")
+new_file.close()
+
+def repair_pdf(input_path, output_path):
+    try:
+        with pikepdf.open(input_path, allow_overwriting_input=True) as pdf:
+            pdf.save(output_path)
+        #print("Repaired PDF saved to:", output_path)
+    except Exception as e:
+        print("Repair failed:", e)
+
+repair_pdf(broken_file, "repaired_pdf.pdf")
