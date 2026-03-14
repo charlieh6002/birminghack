417993f3b178a1215b7195f1d82150e809196ed7
Charlie Haddock <charliehaddock@Charlies-MacBook-Pro.local>
2026-03-14 12:31:48 +0000

    first attempt at window creation

---
+++ config.py
@@ -0,0 +1,7 @@
+import pygame
+pygame.init()
+
+screenWidth = 1080
+screenHeight = 720
+window = pygame.display.set_mode((screenWidth, screenHeight))
+
--- mainProgram.py
+++ mainProgram.py
@@ -1,2 +1,31 @@
+from config import *
+
 import pygame
 pygame.init()
+
+class Game:
+
+    def __init__(self):
+        self.window = window
+
+    def newGame(self):
+        pass
+
+clock = pygame.time.Clock()
+game = Game()
+game.newGame()
+running = True
+
+tick = 0
+
+#------MAIN-LOOP------
+
+while running:
+
+    clock.tick(60)
+    pygame.display.update()
+    for event in pygame.event.get(): #iterates through possible events
+            if event.type == pygame.KEYDOWN: 
+                pass
+            elif event.type == pygame.QUIT:
+                pygame.quit()
