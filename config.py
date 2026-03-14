f405ebab92922f8313aab57cc15c7dd6c3989f76
Charlie Haddock <charliehaddock@Charlies-MacBook-Pro.local>
2026-03-14 12:55:19 +0000

    added assets folder, initialising fonts

--- config.py
+++ config.py
@@ -5,3 +5,14 @@ screenWidth = 1080
 screenHeight = 720
 window = pygame.display.set_mode((screenWidth, screenHeight))
 
+#fonts
+pixeboyLarge = pygame.font.Font("Pixeboy.ttf", 150)
+pixeboyMedium = pygame.font.Font("Pixeboy.ttf", 100)
+pixeboySmall = pygame.font.Font("Pixeboy.ttf", 50)
+
+#images
+
+#title text
+
+titleText = pixeboyMedium.render("file restorer yay")
+titleRect = titleText.get_rect(center = (screenWidth/2, screenHeight/2))
