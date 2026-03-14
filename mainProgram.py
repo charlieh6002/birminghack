67d699725eeb45cc88eb8196424be7e3401dd22b
Erin <thr33rin@DESKTOP-BQJN2V0.localdomain>
2026-03-14 23:50:01 +0000

    has other file types

--- mainProgram.py
+++ mainProgram.py
@@ -30,27 +30,52 @@ def centery(y):
     newy = extray / 2
     return newy
 
-if 0 == 1:
-    try:
-        new_file = open("repaired_pdf.pdf", "r")
-        print("file opened")
-    except FileNotFoundError:
-        new_file = open("repaired_pdf.pdf", "x")
-        print("file created")
-    broken_file = "broken1.pdf"
-    new_file.close()
+#if 0 == 1:
+#    try:
+#        new_file = open("repaired_pdf.pdf", "r")
+#        print("file opened")
+#    except FileNotFoundError:
+#        new_file = open("repaired_pdf.pdf", "x")
+#        print("file created")
+#    broken_file = "broken1.pdf"
+#    new_file.close()
 
-def repair_pdf(input_path, output_path):
-    try:
-        new_file = open(output_path, "x")
-        with pikepdf.open(input_path, allow_overwriting_input=True) as pdf:
-            pdf.save(output_path)
-        #print("Repaired PDF saved to:", output_path)
-        new_file.close()
-    except Exception as e:
-        print("Repair failed:", e)
+#def repair_pdf(input_path, output_path):
+#    try:
+#        new_file = open(output_path, "x")
+#        with pikepdf.open(input_path, allow_overwriting_input=True) as pdf:
+#            pdf.save(output_path)
+#        #print("Repaired PDF saved to:", output_path)
+#        new_file.close()
+#    except Exception as e:
+#        print("Repair failed:", e)
 
 #repair_pdf(broken_file, "repaired_pdf.pdf")
+
+def repairHeader(inPath, outPath, fileType):
+    #PNG header should be 89 50 4E 47  
+    header = b'\x00\x00\x00\x00'
+    if fileType == "png" or fileType == "pdf" or fileType == "jpeg" or fileType == "jfif" : 
+        if fileType == "png":
+            header = b'\x89\x50\x4E\x47'
+        elif fileType == "pdf":
+            header = b'\x25\x50\x44\x46'
+        elif fileType == "jpeg" or fileType == "jfif":
+            header = b'\xFF\xD8\xFF\xE0' 
+    else : 
+        raise ValueError("Not a suppported fileType")
+
+    with open(inPath, 'rb') as file: # opens a file called 'file' in the input location and reads it as binary, closes the file immediately after 
+        content = file.read() #stores the bytes of the file in content 
+    
+    if len(content) < 4:
+        raise ValueError("File is too small to be a PNG")
+
+    fixedHeader = header + content[4:]
+
+    with open(outPath, 'wb') as file:
+        file.write(fixedHeader)
+    #print("Success!  file saved to: " + outPath)
 
 screen = 1
 loadingTick = 0
@@ -110,11 +135,17 @@ while running:
             if event.type == pygame.DROPFILE:
                 channel1.play(loadingSound)
                 corruptedFile = event.file
-                print(corruptedFile)
-                splitFile = corruptedFile.split("/")
-                newFileName = splitFile[len(splitFile) - 1]
-                newFileName = newFileName[:-4] + "-REPAIRED.pdf"
+                #print(corruptedFile)
+                splitFile = corruptedFile.split("/") #filename.filetype 
+                newFileName = splitFile[len(splitFile) - 1] 
+                corruptFileType = newFileName[1] 
+                newFileName = newFileName[:-4] + "-REPAIRED." + corruptFileType
+                repairHeader(corruptedFile, newFileName, corruptFileType)
                 screen = 2
-                repair_pdf(corruptedFile, newFileName)
+                #repair_pdf(corruptedFile, newFileName)
             elif event.type == pygame.QUIT:
                 pygame.quit()
+
+
+#add something to select the filetype which is filename[-4:] 
+#add other filetype corrections to the function
